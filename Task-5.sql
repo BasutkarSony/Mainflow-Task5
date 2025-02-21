27bb7a2a66b1e033247fbc9115b1797ed5f518ba
BasutkarSony <basutkarsony14@gmail.com>
2025-02-21 14:59:33 +0530

    Add files via upload

---
+++ Task-5.sql
@@ -0,0 +1,31 @@
+Create table StudentScores(
+StudentId int, 
+TotalScore int, 
+MathScore int,
+ScienceScore int
+);
+
+Insert into StudentScores(StudentId, TotalScore, Mathscore,ScienceScore) values 
+(1,95,45,50),
+(2,85,35,60),
+(3,75,40,30),
+(4,65,25,20);
+
+select StudentId, TotalScore, case
+	when TotalScore >=90 then 'A'
+    when TotalScore >=80 then 'B'
+    when TotalScore >=70 then 'C'
+    else 'D (Fail)'
+    end as Grade
+from StudentScores;
+
+select StudentId,MathScore, ScienceScore,
+	case 
+		when MathScore>=40 then 'Pass'
+        else 'Fail'
+	end as MathStatus,
+    case
+		when ScienceScore>=40 then 'Pass'
+        else 'Fail'
+	end as ScienceStatus
+from StudentScores;
